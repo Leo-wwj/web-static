85df3935e839667a1e74f8857c3cde9b3b6beaab
Leo-wwj <719405667@qq.com>
2019-09-15 16:50:29 +0800

    修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,18 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'max-len': ['error', 120],
+    'arrow-parens': ['error', 'as-needed'],
+    'func-names': ['error', 'as-needed'],
+    import/extensions': [
+    'error',
+    'always',
+    {
+    js: 'never',
+    },
+    ],
+    'no-param-reassign': ['error', { props: false }],
   },
   parserOptions: {
     parser: "babel-eslint"
